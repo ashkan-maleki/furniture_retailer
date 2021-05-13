5e4a4ca823d4e7495db6bbde2a236fe70fe49b9c
johnnywcage <johnnywcage@outlook.com>
2021-05-13 16:49:14 +0430

    modifing orm tests configs

--- conftest.py
+++ conftest.py
@@ -8,13 +8,13 @@ from orm import metadata, start_mappers
 
 @pytest.fixture
 def in_memory_db():
-    engine = create_engine('sqllite:///:memory:')
+    engine = create_engine("sqlite:///:memory:")
     metadata.create_all(engine)
     return engine
+
 
 @pytest.fixture
 def session(in_memory_db):
     start_mappers()
     yield sessionmaker(bind=in_memory_db)()
     clear_mappers()
-
