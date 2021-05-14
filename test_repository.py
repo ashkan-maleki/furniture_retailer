b7a5187622805d8c26e31edc6a5e311f0842d2cf
johnnywcage <johnnywcage@outlook.com>
2021-05-14 13:02:02 +0430

    tests for repository

--- test_repository.py
+++ test_repository.py
@@ -16,7 +16,7 @@ def test_repository_can_save_a_batch(session: Session):
     assert list(rows) == [("batch1", "RUSTY-SOAPDISH", 100, None)]
 
 
-def insert_order_line(session):
+def insert_order_line(session : Session):
     session.execute(
         "INSERT INTO order_lines (orderid, sku, qty)"
         ' VALUES ("order1", "GENERIC-SOFA", 12)'
@@ -25,6 +25,7 @@ def insert_order_line(session):
         "SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku",
         dict(orderid="order1", sku="GENERIC-SOFA"),
     )
+
     return orderline_id
 
 
