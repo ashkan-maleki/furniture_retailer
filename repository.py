5f81f1d459e3988531f6484a3330567a73aca738
johnnywcage <johnnywcage@outlook.com>
2021-05-14 13:00:40 +0430

    sqlalchemy repository is added

--- repository.py
+++ repository.py
@@ -1,5 +1,7 @@
 import abc
 
+from sqlalchemy.orm import Session
+
 import model
 
 
@@ -11,3 +13,17 @@ class AbstractRepository(abc.ABC):
     @abc.abstractmethod
     def get(self, reference: str) -> model.Batch:
         raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session: Session):
+        self.session: Session = session
+
+    def add(self, batch: model.Batch) -> None:
+        self.session.add(batch)
+
+    def get(self, reference: str):
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(model.Batch).all()
